647228ad18220d80665f539f9606262d40f91c07
JohnCdf <diaz.john312@gmail.com>
2018-05-30 17:28:02 -0400

    m

--- index.js
+++ index.js
@@ -20,14 +20,6 @@ const routes = {
   '/api/auth' : handlers.auth
 };
 
-app.use(function (req, res, next) {
-res.setHeader('Access-Control-Allow-Origin', '*');
-res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-res.setHeader('Access-Control-Allow-Credentials', true);
-next();
-});
-
 app.all('/*', ( req, res ) => {
   let pathQuery = url.parse(req.url, true);
   let pathname = pathQuery.pathname;
