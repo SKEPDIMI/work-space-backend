8d362db5c34c5aec3140634f44b15b4b20de6217
John S <diaz.john312@gmail.com>
2018-06-30 19:14:07 -0400

    Added ability to upload images and update profile

--- index.js
+++ index.js
@@ -12,46 +12,56 @@ var multer  = require('multer');
 
 var app = express();
 
-var connection = mongoose.connect(`mongodb:\/\/${config.DBusername}:${config.DBpassword}@ds235860.mlab.com:35860/work-space`)
-var PORT = process.env.PORT || 5000;
+mongoose.connect(`mongodb:\/\/${config.DBusername}:${config.DBpassword}@ds235860.mlab.com:35860/work-space`);
 
+app.use(express.json());
 app.use(cors())
 app.options('*', cors())
 
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
+app.disable( 'x-powered-by' );
+
+const upload = multer ({
+  dest: path.join(__dirname + '/uploads')
+});
 
 const routes = {
   '/api/users' : handlers.users,
   '/api/user' : handlers.user,
   '/api/spaces' : handlers.spaces,
-  '/api/auth' : handlers.auth
+  '/api/auth' : handlers.auth,
+  '/api/user/image' : handlers.userImage
 };
 
-app.all('*', ( req, res ) => {
+app.all('*', upload.array('avatar'), ( req, res ) => { // All requests are passed onto handlers
   let pathQuery = url.parse(req.url, true);
   let pathname = pathQuery.pathname;
+  let query = pathQuery.query;
   let method = req.method;
 
   let handler = routes[pathname] ? routes[pathname] : handlers.notfound;
 
-          let data = {
-            pathname: pathname,
-            method: method.toLowerCase(),
-            headers: req.headers || {},
-            body: req.body
-          };
-          handler(data, ( statusCode, payload, contentType ) => {
-            res.status(statusCode);
-            res.contentType(contentType);
-            if (contentType == 'application/json') {
-              res.json(payload)
-            } else {
-              res.send(payload)
-            }
-          })
+  let data = {
+    pathname: pathname,
+    method: method.toLowerCase(),
+    headers: req.headers || {},
+    body: req.body || {},
+    files: req.files || {},
+    query: query || []
+  };
+
+  handler(data, ( statusCode = 418, payload = {}, contentType = 'text/plain' ) => { // And the response from these handlers is sent back
+    res.status(statusCode);
+    res.contentType(contentType);
+
+    if (contentType == 'application/json') {
+      res.json(payload)
+    } else {
+      res.send(payload)
+    }
+  })
 });
 
 morgan('tiny');
 
+var PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>console.log('port: '+PORT))
