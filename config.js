3b51437dffeed1428fabe005d8722101aac43f9b
John S <diaz.john312@gmail.com>
2018-07-02 13:20:03 -0400

    Update user stuff

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 module.exports  ={
-  jwtKey : 'if5jfvi4',//the key we use to encrypt jwt tokens
+  jwtKey : 'if5jfvi4', // the key we use to encrypt jwt tokens
   
   DBusername: 'admin',
-  DBpassword: '8ce70ac8ece252cc48a2fb875fd69f589c0ba0e53cc62c76241e81529dfb2b1f'
+  DBpassword: '8ce70ac8ece252cc48a2fb875fd69f589c0ba0e53cc62c76241e81529dfb2b1f',
+
+  salt: 10
 }
